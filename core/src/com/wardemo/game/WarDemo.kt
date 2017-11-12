a296748ebff1cf8e87720bdc882780ee4a9e407d
KuzminKirill <wee3dood@gmail.com>
2017-11-12 16:05:37 +0300

    replace

--- core/src/com/wardemo/game/WarDemo.kt
+++ core/src/com/wardemo/game/WarDemo.kt
@@ -13,10 +13,11 @@ class WarDemo : ApplicationAdapter() {
     override fun create() {
         batch = SpriteBatch()
         img = Texture("badlogic.jpg")
+        Gdx.gl.glClearColor(1f, 0f, 0f, 1f)
     }
 
     override fun render() {
-        Gdx.gl.glClearColor(1f, 0f, 0f, 1f)
+
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT)
         batch.begin()
         batch.draw(img, 0f, 0f)
