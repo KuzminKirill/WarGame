d7eddf3420141d50338b7423c060d67b6369478d
KuzminKirill <wee3dood@gmail.com>
2017-11-26 23:29:30 +0300

    add pics

--- core/src/com/wardemo/game/states/LevelState.kt
+++ core/src/com/wardemo/game/states/LevelState.kt
@@ -2,17 +2,23 @@ package com.wardemo.game.states
 
 
 import com.badlogic.gdx.Gdx
+import com.badlogic.gdx.graphics.Texture
 import com.badlogic.gdx.graphics.g2d.SpriteBatch
 import com.wardemo.game.WarDemo
 import com.wardemo.game.core.Hero
 
+
 class LevelState(gsm :GameStateManager) : State(gsm) {
 
     var hero : Hero
+    private val leftBtn: Texture
+    private val rightBtn: Texture
 
     init {
         hero = Hero(50f,50f)
         camera.setToOrtho(false,WarDemo.WIDTH / 2f, WarDemo.HEIGHT / 2f)
+        leftBtn = Texture("left.png")
+        rightBtn = Texture("right.png")
     }
     override fun handleInput() {
         if (Gdx.input.justTouched()) {
@@ -26,10 +32,15 @@ class LevelState(gsm :GameStateManager) : State(gsm) {
         hero.update(dt)
     }
 
+
+
     override fun render(sb: SpriteBatch) {
         sb.projectionMatrix = camera.combined
         sb.begin()
+        sb.draw(leftBtn, 0f, 0f)
+        sb.draw(rightBtn, 25f, 0f)
         sb.draw(hero.hero, hero.position.x,hero.position.y)
+
         sb.end()
     }
 
