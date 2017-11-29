f023971d8fe673d61835bb1be9a4ec331a10d6fd
Letov <letov.nikolay@gmail.com>
2017-11-29 09:06:43 +0300

    created jump rule, created coin and his physics created score message

--- core/src/com/wardemo/game/states/LevelState.kt
+++ core/src/com/wardemo/game/states/LevelState.kt
@@ -2,21 +2,30 @@ package com.wardemo.game.states
 
 
 import com.badlogic.gdx.Gdx
+import com.badlogic.gdx.graphics.GL20
 import com.badlogic.gdx.graphics.Texture
+import com.badlogic.gdx.graphics.g2d.BitmapFont
 import com.badlogic.gdx.graphics.g2d.SpriteBatch
 import com.wardemo.game.WarDemo
+import com.wardemo.game.core.Coin
 import com.wardemo.game.core.Hero
 
 
 class LevelState(gsm :GameStateManager) : State(gsm) {
 
     var hero : Hero
+    var coin : Coin
     private val leftBtn: Texture
     private val rightBtn: Texture
+    private var font : BitmapFont
+    var score : Int
 
     init {
         hero = Hero(50f,50f)
+        coin = Coin( 100f, 200f)
         camera.setToOrtho(false,WarDemo.WIDTH / 2f, WarDemo.HEIGHT / 2f)
+        score = 0
+        font = BitmapFont()
         leftBtn = Texture("left.png")
         rightBtn = Texture("right.png")
     }
@@ -30,16 +39,25 @@ class LevelState(gsm :GameStateManager) : State(gsm) {
     override fun update(dt: Float) {
         handleInput()
         hero.update(dt)
+        coin.update(dt)
     }
 
 
 
     override fun render(sb: SpriteBatch) {
-        sb.projectionMatrix = camera.combined
+
+        Gdx.gl.glClearColor(0f, 0f, 0.2f, 1f)
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT)
+        camera.update()
+
+
+       // sb.projectionMatrix = camera.combined
         sb.begin()
+        font.draw(sb, "SCORE: " + score, 0f,400f)
         sb.draw(leftBtn, 0f, 0f)
         sb.draw(rightBtn, 25f, 0f)
-        sb.draw(hero.hero, hero.position.x,hero.position.y)
+        sb.draw(hero.hero, hero.position.x, hero.position.y)
+        sb.draw(coin.coin, coin.position.x, coin.position.y)
 
         sb.end()
     }
